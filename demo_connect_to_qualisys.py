1a810e7fcf884c653ff4f0df68f0e6590944fe32
ECEEvanCampbell <evan.campbell1@unb.ca>
2023-06-28 17:31:44 +0200

    gpt4 version

--- demo_connect_to_qualisys.py
+++ demo_connect_to_qualisys.py
@@ -1,26 +1,27 @@
+import asyncio
+import qtm_rt
+from multiprocessing import Process
+from multiprocessing import set_start_method
 
 # Make sure you've installed the Qualisys 
 # https://github.com/qualisys/qualisys_python_sdk
 QUALISYS_IP = "192.168.50.50"
 
-from multiprocessing import Process
-import qtm_rt
-import asyncio
 
 # this is the context sever
 class ContextServer:
     def __init__(self):
-        self.process = Process(target = self.grab_packets, daemon=True,)
+        self.process = None
 
     async def run(self, block=True):
         if block:
             await self.grab_packets()
         else:
-            await self.process.start()
-    
+            self.process = Process(target=self.start_new_loop)
+            self.process.start()
+
     async def connect(self):
         self.connection = await qtm_rt.connect(QUALISYS_IP)
-
 
     async def grab_packets(self):
         # closure
@@ -30,13 +31,34 @@ class ContextServer:
             for marker in markers:
                 print("\t", marker)
         await self.connection.stream_frames(components=["6d"], on_packet=on_packet)
-    
+
+    def start_new_loop(self):
+        loop = asyncio.new_event_loop()
+        asyncio.set_event_loop(loop)
+        while True:
+            try:
+                # Create the connection in the child process instead
+                connection = loop.run_until_complete(qtm_rt.connect(QUALISYS_IP))
+                # Use the connection for the `stream_frames` method
+                loop.run_until_complete(connection.stream_frames(components=["6d"], on_packet=self.on_packet))
+            except Exception as e:
+                print(e)
+        #loop.close()
+
+    # No longer a closure, so it can be pickled
+    def on_packet(self, packet):
+        header, markers = packet.get_6d()
+        print("Component info: {}".format(header))
+        for marker in markers:
+            print("\t", marker)
+
 
 async def main():
     cs = ContextServer()
-    await cs.connect()
-    await cs.run(block=False)  
+    await cs.run(block=False)
+
 
 if __name__ == "__main__":
-    asyncio.ensure_future(main())
-    asyncio.get_event_loop().run_forever()
+    # Use 'spawn' method to start a fresh Python interpreter for each child process
+    set_start_method('spawn')
+    asyncio.run(main())
