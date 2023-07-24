bbce090f74694a63b9b711c28442e3737098e0fa
ECEEvanCampbell <evan.campbell1@unb.ca>
2023-07-24 11:32:28 +0200

    added reset for files

--- qualysis_main.py
+++ qualysis_main.py
@@ -1,7 +1,7 @@
 import time
 import asyncio
 import qtm_rt
-
+import os
 QUALISYS_IP = "192.168.50.50" 
 
 def on_packet(packet):
@@ -34,4 +34,10 @@ def main(start_perf_counter):
     asyncio.get_event_loop().run_forever()
 
 if __name__ == "__main__":
+    # cleanup
+    if os.path.exists("Data/copter.csv"):
+        os.remove("Data/copter.csv")
+    if os.path.exists("Data/turtlebot.csv"):
+        os.remove("Data/turtlebot.csv")
+    # begin logging
     main(time.perf_counter())
